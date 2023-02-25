fc33835ff0bd6356dec1a86b964544c720555958
Wachirapol Klinkasorn <wachirapol2545@gmail.com>
2023-02-26 04:57:21 +0700

    Bug: localStorage

---
+++ condb.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "noneq_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+// $sql = "CREATE DATABASE myDB";
+// if ($conn->query($sql) === TRUE) {
+//   echo "Database created successfully";
+// } else {
+//   echo "Error creating database: " . $conn->error;
+// }
+
+// $conn->close();
+?>
