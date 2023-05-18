93d8f349fa77dfa6e91965af6e0904db9723bed0
PongpunSaengphon <pp_saeng@hotmail.com>
2023-05-18 21:16:38 +0700

    Home

--- noneq/src/views/Home.vue
+++ noneq/src/views/Home.vue
@@ -1,9 +1,28 @@
 <template>
-    <div id="app">
-        <div class="container-fluid" :class="center" style="background-color: rgb(84, 84, 84);">
-            <img style="width: 47%;" :src="require('../assets/NoneQ.png')" alt="">
-        </div>
+  <div id="app">
+    <div class="container-fluid d-flex flex-column justify-content-center align-items-center"
+      style="min-height: 90vh; background-color: rgb(84, 84, 84);">
+
+      <div class="d-flex flex-column align-items-center fw-bold text-white">
+        <p class="h1 position-absolute" style=" font-size: 70px;"><i>W e l c o m e</i><i class="fa-solid fa-fork"></i></p>
+        <img :src="require('@/assets/NoneQ.png')" style="width: 650px; border-radius: 50%;">
+        
+
+
+        <div class="position-absolute" style="border-top: 5px solid white; border-bottom: 5px solid white; margin-top: 450px;">
+          <router-link to="">
+          <button class="btn fw-bold text-white ps-5 pe-5 " style="font-size: 60px;"
+          :style="{backgroundColor: isHover ? 'rgb(110, 110, 110, 0.5)': 'rgb(84, 84, 84)'}" @mouseover="isHover = true"
+          @mouseleave="isHover = false">
+            <i><i class="bi bi-chevron-double-right"></i> NoneQ Store <i class="bi bi-chevron-double-left"></i></i>
+            <i><p class="h6">Enter Here</p></i>
+          </button>
+        </router-link>
+      </div>
     </div>
+  </div>
+
+  </div>
 </template>
 
 <script>
@@ -13,13 +32,14 @@ export default {
   name: "HomePage",
   data() {
     return {
+      isHover: false,
       search: "",
       tables: [],
-      center:{
-      'd-flex': true,
-      'justify-content-center':true,
-      'align-items-center':true
-    },
+      center: {
+        'd-flex': true,
+        'justify-content-center': true,
+        'align-items-center': true
+      },
     };
   },
   mounted() {
@@ -28,10 +48,10 @@ export default {
   methods: {
     getTables() {
       axios.get("http://localhost:3000", {
-          params: {
-            search: this.search
-          }
-        })
+        params: {
+          search: this.search
+        }
+      })
         .then(response => {
           this.tables = response.data;
         })
@@ -42,3 +62,5 @@ export default {
   }
 };
 </script>
+
+
