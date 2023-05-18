804168c486d4a06c570bca001ad696828e480f02
PongpunSaengphon <pp_saeng@hotmail.com>
2023-05-19 02:34:16 +0700

    home update

--- noneq/src/views/Home.vue
+++ noneq/src/views/Home.vue
@@ -1,32 +1,49 @@
 <template>
   <div id="app">
-    <div class="container-fluid d-flex flex-column justify-content-center align-items-center"
-      style="min-height: 90vh; background-color: rgb(84, 84, 84);">
 
-      <div class="d-flex flex-column align-items-center fw-bold text-white">
-        <p class="h1 position-absolute" style=" font-size: 70px;"><i>W e l c o m e</i><i class="fa-solid fa-fork"></i></p>
+    <MDBCarousel
+    v-model="carousel3"
+    :items="items3"
+    :indicators="false"
+  />
+    <div class="position-absolute">
+      <div class="position-inline">
+      <img :src="require('@/assets/res_pic1.jpg')" style="width: 950px; height: 870px; opacity: 0.8;">
+      <img :src="require('@/assets/res_pic2.jpg')" style="width: 950px; height: 870px; opacity: 0.8;">
+    </div>
+    </div>
+    
+
+    <div class="position-absolute container-fluid d-flex flex-column justify-content-center align-items-center "
+      style="min-height: 90vh;">
+      <div class="d-flex flex-column align-items-center fw-bold text-white" style="background-color: rgb(84, 84, 84);
+       padding-bottom: 170px; padding-top: 50px; border-left: 8px solid white ; border-right: 8px solid white;">
+        <p class="h1 position-absolute" style=" font-size: 70px;"><i>W e l c o m e  </i></p>
         <img :src="require('@/assets/NoneQ.png')" style="width: 650px; border-radius: 50%;">
-        
 
-
-        <div class="position-absolute" style="border-top: 5px solid white; border-bottom: 5px solid white; margin-top: 450px;">
+        <div class="position-absolute"
+          style="border-top: 5px solid white; border-bottom: 5px solid white; margin-top: 450px;">
           <router-link to="">
-          <button class="btn fw-bold text-white ps-5 pe-5 " style="font-size: 60px;"
-          :style="{backgroundColor: isHover ? 'rgb(110, 110, 110, 0.5)': 'rgb(84, 84, 84)'}" @mouseover="isHover = true"
-          @mouseleave="isHover = false">
-            <i><i class="bi bi-chevron-double-right"></i> NoneQ Store <i class="bi bi-chevron-double-left"></i></i>
-            <i><p class="h6">Enter Here</p></i>
-          </button>
-        </router-link>
+            <button class="btn fw-bold text-white ps-5 pe-5 " style="font-size: 60px;"
+              :style="{ backgroundColor: isHover ? 'rgb(110, 110, 110, 0.5)' : 'rgb(84, 84, 84)' }"
+              @mouseover="isHover = true" @mouseleave="isHover = false">
+              <i><i class="bi bi-chevron-double-right"></i> NoneQ Store <i class="bi bi-chevron-double-left"></i></i>
+              <i>
+                <p class="h6">Enter Here</p>
+              </i>
+            </button>
+          </router-link>
+        </div>
       </div>
     </div>
-  </div>
 
   </div>
 </template>
 
 <script>
+
 import axios from "axios";
+
 // @ is an alias to /src
 export default {
   name: "HomePage",
@@ -60,7 +77,6 @@ export default {
         });
     },
   }
+  
 };
 </script>
-
-
