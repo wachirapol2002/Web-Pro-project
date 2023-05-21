dd91d49bda9bf2082b7872e696bd5c2d9b9f9a96
PongpunSaengphon <pp_saeng@hotmail.com>
2023-05-21 17:29:28 +0700

    Order Update + Database

--- noneq/src/views/Order.vue
+++ noneq/src/views/Order.vue
@@ -6,11 +6,12 @@
                 <div class="container is-max-desktop">
                     <div class="is-multiline columns is-variable is-2">
 
-                        <div id="card_product" class="column is-one-quarter" v-for="product in menus" :key="product.menu_id">
+                        <div id="card_product" class="column is-one-quarter" v-for="product in menus"
+                            :key="product.menu_id">
                             <div class="card">
-                                <div class="card-image" >
-                                    <figure class="image is-1by1" >
-                                        <img :src="'http://localhost:3000'+product.menu_pic" alt="Placeholder image"/>
+                                <div class="card-image">
+                                    <figure class="image is-1by1">
+                                        <img :src="'http://localhost:3000' + product.menu_pic" alt="Placeholder image" />
                                     </figure>
                                 </div>
                                 <div class="card-content">
@@ -21,7 +22,7 @@
                                         </div>
                                     </div>
                                     <div style="display: flex;">
-                                        <input type="button" class="btn btn-dark fw-bold fs-5" value="เพิ่มลงรายการอาหาร"
+                                        <input type="button" class="btn btn-dark fw-bold fs-5" value="Add to Order"
                                             @click="addToOrder(product)">
                                     </div>
                                 </div>
@@ -37,32 +38,35 @@
             <div class="column is-3 pt-6 pl-0 pr-5">
                 <div style="display: flex;justify-content: space-between;">
                     <span class="title mb-4 fw-bold fs-3">รายการอาหารที่สั่ง</span>
-                    <a @click="setZero()" class=" mb-4 button" style="text-decoration: none;">Clear</a>
+                    <a @click="setZero(product)" class=" mb-4 button fw-bold" style="text-decoration: none;">Clear</a>
                 </div>
                 <div class="card mb-4">
                     <div v-for="(product, index) in order" :key="index" class="card-content p-0">
                         <div class="media">
                             <figure class="image is-96x96 pt-3">
-                                <img :src="'http://localhost:3000'+product.menu_pic" alt="Placeholder image"/>
+                                <img :src="'http://localhost:3000' + product.menu_pic" alt="Placeholder image" />
                             </figure>
-                            <div class="media-content pt-4 fw-bold">
+                            <div class="media-content pt-4 fw-bold fs-5">
                                 {{ product.menu_name }}
                                 <div style="display: flex;justify-content: space-between;">
-                                    <div class="fw-bold ">
-                                        X 0
+                                    <div class="fw-bold fs-5">
+                                        x {{ product.menu_quantity }}
                                     </div>
-                                    <div>
-                                        <a class="button me-5" style="text-decoration: none;" @click="removeFromOrder(product, index)">Delete</a>
+                                    <div class="d-flex flex-column justify-content-center align-items-center">
+                                        <input class="button me-5 fw-bold" style="text-decoration: none;"
+                                            @click="removeFromOrder(product, index)" value="Delete">
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <hr>
                     </div>
-                        <label class="form-label fw-bold fs-5 mt-3 ms-2" for="date">ราคารวม : </label>
-                    <a class="button is-dark mt-3 fw-bold fs-4" style="width: 100%;color: white; text-decoration: none;"
-                        @click="menuConfirm()">สั่งอาหาร</a>
+                    <label class="form-label fw-bold fs-5 mt-3 ms-2" v-if="showPrice">ราคารวม : {{ sum_price }} บาท</label>
+                    <input class="button is-dark mt-3 fw-bold fs-3" style="width: 100%;color: white; text-decoration: none;"
+                        @click="menuConfirm()" value="Ordered">
                 </div>
             </div>
+            <hr>
             <!-- ---------------------------------------------------------- -->
         </div>
 
@@ -81,7 +85,8 @@ export default {
         return {
             menus: [],
             order: [],
-            quantity: 0,
+            sum_price: 0,
+            showPrice: false,
         };
     },
     methods: {
@@ -96,22 +101,70 @@ export default {
         },
 
         addToOrder(item) {
-            if (this.quantity == 0) {
+            this.showPrice = true;
+            if (item.menu_quantity == 0) {
                 this.order.push(item)
-                item.quantity++
+                item.menu_quantity++
             }
-            else if (this.quantity > 0) {
-                item.quantity++
+            else if (item.menu_quantity > 0) {
+                item.menu_quantity++
             }
+            this.sum_price += item.menu_price
 
         },
+
+        removeFromOrder(item, indexs) {
+            if (item.menu_quantity > 1) {
+                item.menu_quantity--
+
+            }
+            else if (item.menu_quantity <= 1) {
+                item.menu_quantity = 0
+                this.order.splice(indexs, 1)
+            }
+            this.sum_price -= item.menu_price
+
+            if(this.sum_price == 0){
+                this.showPrice = !this.showPrice;
+            }
+        },
+
+        setZero() {
+            this.order.forEach((item) => {
+                item.menu_quantity = 0;
+            })
+            this.order = []
+            this.sum_price = 0;
+            this.showPrice = !this.showPrice;
+        },
+
         menuConfirm() {
+
             if (this.order == "") {
                 alert("กรุณาสั่งรายการอาหาร")
             }
             else {
+                const result = window.confirm('ยืนยันที่จะสั่งรายการอาหารหรือไม่?');
+                if (result) {
+                    if (result) {
+                        alert("รายการอาหารถูกสั่งเรียบร้อย")
+                        console.log('saved')
+                        this.$router.push({ path: '/confirm' })
+                    } else {
+                        alert('คุณยกเลิกแล้ว');
+                    }
 
-                // const data = {
+                }
+
+            }
+        },
+    },
+    mounted() {
+        this.getMenu();
+    },
+
+};
+ // const data = {
                 //     order_details_id: "",
                 //     order_id: "",
                 //     menu_id: "",
@@ -126,16 +179,5 @@ export default {
                 //         console.log(err);
                 //     });
 
-                alert("รายการอาหารถูกสั่งเรียบร้อย")
-                console.log('saved')
-                this.$router.push({ path: '/confirm' })
-            }
-        },
-    },
-    mounted() {
-        this.getMenu();
-    },
-
-};
 </script>
   
