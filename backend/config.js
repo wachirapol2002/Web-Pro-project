e5b75d560cc4085d9b5f80fa1c45b9d9422a7abc
Wachirapol Klinkasorn <wachirapol2545@gmail.com>
2024-03-12 22:57:27 +0700

    change DB

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: 'http://noneq.cnygwcwe08j9.us-east-1.rds.amazonaws.com/',
+  user: 'admin',
+  password: 'password',
   database: 'noneq',
   waitForConnections: true,
   connectionLimit: 10,
