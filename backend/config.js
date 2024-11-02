a314cc241405ce175aba664666c83f7974f8b943
Wachirapol Klinkasorn <wachirapol2545@gmail.com>
2024-11-02 23:57:25 +0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,19 @@
 const mysql = require('mysql2/promise');
 
+// const pool = mysql.createPool({
+//   host: 'noneq.cnygwcwe08j9.us-east-1.rds.amazonaws.com',
+//   user: 'admin',
+//   password: 'password',
+//   database: 'noneq',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
 const pool = mysql.createPool({
-  host: 'noneq.cnygwcwe08j9.us-east-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'password',
+  host: 'localhost',
+  user: 'root',
+  password: '',
   database: 'noneq',
   waitForConnections: true,
   connectionLimit: 10,
